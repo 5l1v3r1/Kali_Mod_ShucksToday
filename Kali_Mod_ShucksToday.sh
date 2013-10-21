395685c8dbe7acaded12c4519d1878bd074fdd77
Mushin No Mushin <mushinnomushin@gmail.com>
2013-10-20 19:00:39 -0500

    The old version is crap. This is a much more organized version that will allow for better logic & more functions.

--- Kali_Mod_ShucksToday.sh
+++ Kali_Mod_ShucksToday.sh
@@ -1,66 +1,84 @@
-#! /bin/bash
+#!/bin/bash
 
-# Update & upgrade, of course
+##Ask some basic questions.
+
+##print "Is this a basic install of Kali?"
+##print "Would you like to add the "Bleeding Edge" repositories to Kali?"
+
+##First Run: Add the "Bleeding Edge" repositories.
+
+##echo deb http://repo.kali.org/kali kali-bleeding-edge main >> /etc/apt/sources.list
+
+##Get some updates...
+
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 
-# Install the Bleeding Edge repos
-echo deb http://repo.kali.org/kali kali-bleeding-edge main >> /etc/apt/sources.list
+##First Run: Next, we add ALL of Kali.
 
-# Update & upgrade now that the Bleeding Edge repos are installed
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
+apt-get install -y kali-linux-all
 
-# Apt-get drivers & such for my laptop (ThinkPad T61p)
-#apt-get -y install hdapsd tp-smapi-dkms nvclock nvclock-gtk (this isn't finished)
+##Desktop & Laptop: Kali Live Build environment.
 
-# Apt-get shit that Kali needs
-apt-get -y install icedove p7zip build-essential libqt4-dev qt4-doc qt4-dev-tools mtr secure-delete wipe xfs-progs python-pip libyaml mingw32 sqlmap bully lbd automater arachni ubertooth wce lib32ncurses5 ia32-libs nipper-ng python-software-properties tpb unetbootin strace basket network-manager-openvpn-gnome network-manager-pptp-gnome network-manager-vpnc-gnome gkrellm gkrellm-thinkbat gkrellmitime truecrack python-scapy beef-xss htop arp-scan kernel-package libncurses5-dev live-build cdebootstrap apt-cacher-ng gns3 chirp gpredict cdck automake1.9 checkinstall libcompfaceg1 libcompfaceg1-dev libssl-dev libssl-doc
+apt-get install -y git live-build cdebootstrap kali-archive-keyring
 
-# Add gems
+##Adding some programs that I like in every Linux distro :)
+
+apt-get install -y vim-nox vim-doc mtr strace wipe secure-delete
+
+##Add some programs I like in Kali ;)
+
+apt-get -y masscan
+
+##Laptop & Desktop: Minor program additions to make working easier.
+
+apt-get -y install icedove
+
+##Some other ham radio stuff - radios are fun!
+
+apt-get install -y chirp
+
+##We add some gems.
+
 gem install fuzzbert ronin ronin-asm ronin-dorks ronin-exploits ronin-gen ronin-grid ronin-php ronin-scanners ronin-sql ronin-support ronin-web
 
-# Create /opt if it's not already there && fucking switch to it
-mkdir /opt
-cd /opt
+##Comment these lines out if you are not using a laptop.
 
-# Wget some projects; it may be necessary to update the download links (especial# ly for new versions)
+apt-get install -y laptop-mode-tools
+
+##Comment these lines out if you don't have an NVidia graphics card - this doesn't work yet; the installation for winexe needs to be fixed.
+
+#apt-get install -y nvclock nvclock-gtk
+
+##Getting OpenVAS set up - not done yet.
+
+#sh -c "openvas-check-setup;${SHELL:-bash}"
+#sh -c "openvas-setup;${SHELL:-bash}"
+
+##Make personal /opt, /virtual_machines, & /git directories. 
+
+mkdir /root/opt
+mkdir /root/virtual_machines
+mkdir /root/git
+
+##Here is where you perform a "git pull" to grab your personal repositories.
+
+cd /root/git
+
+##Let's download some shit.
+
+cd /root/opt
+
+# Wget some projects; it may be necessary to update the download links (especially for new versions)
+
 wget wifipineapple.com/wp4.sh
 wget https://pwn-star.googlecode.com/files/PwnSTAR_0.9.tgz
 wget https://www.cookiecadger.com/files/CookieCadger-1.0.jar
 wget http://www.sptoolkit.com/?aid=2755&sa=1
 wget http://subterfuge.googlecode.com/files/SubterfugePublicBeta5.0.tar.gz
 
-# Subversion some projects...
-svn checkout http://proxmark3.googlecode.com/svn/trunk pm3
+##Cleanup unnecessary stuff.
 
-# & Make Git requests for other projects
-git clone git://git.kali.org/live-build-config.git
-git clone https://bitbucket.org/LaNMaSteR53/recon-ng.git
-git clone https://github.com/zcutlip/bowcaster.git
-git clone https://github.com/ChrisTruncer/Veil.git
-
-# Now to untar stuff
-tar xvjf devkitARM_r41-i686-linux.tar.bz2 devkitpro/
-tar xvzf PwnSTAR_0.9.tgz
-tar xfvz Wi-fEye-v0.5.6.tar.gz
-tar xvfz OWASP\ Mantra\ Janus\ Linux\ 64.tar.gz
-tar xfvz SubterfugePublicBeta5.0.tar.gz
-
-# Changing permissions on simple stuff
-chmod +x wp4.sh
-chmod +x CookieCadger-1.0.jar
-
-# Untar & install Subterfuge
-
-# Removing unneccessary files
-rm devkitARM_r41-i686-linux.tar.bz2 devkitpro/
-rm PwnSTAR_0.9.tgz
-rm Wi-fEye-v0.5.6.tar.gz
-rm OWASP\ Mantra\ Janus\ Linux\ 64.tar.gz
-rm SubterfugePublicBeta5.0.tar.gz
-
-# Update the locate database becasue fuck find
+apt-get -y autoremove
 updatedb
