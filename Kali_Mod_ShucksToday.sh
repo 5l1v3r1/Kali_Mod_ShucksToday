828ca36ee39a0e1e8c4f79c94a3c9033f8b3636b
Mushin No Mushin <mushinnomushin@gmail.com>
2013-10-27 04:37:04 -0500

    Added some tools like FuzzDB & started fixing some dependencies.

--- Kali_Mod_ShucksToday.sh
+++ Kali_Mod_ShucksToday.sh
@@ -37,7 +37,7 @@ apt-get install -y masscan
 
 ## Laptop & Desktop: Minor program additions to make working easier. This is tested & working.
 
-apt-get -y install icedove basket
+apt-get -y install icedove basket pidgin pidgin-extprefs pidgin-hotkeys pidgin-openpgp pidgin-otr pidgin-plugin-pack pidgin-privacy-please
 
 ## Some other ham radio stuff - radios are fun! This is tested & working.
 
@@ -64,6 +64,8 @@ apt-get install -y laptop-mode-tools
 ## Make personal /opt, /virtual_machines, & /git directories. 
 
 mkdir /root/opt
+mkdir /root/opt/configs
+mkdir /root/opt/configs/certs
 mkdir /root/virtual_machines
 mkdir /root/git
 
@@ -75,34 +77,52 @@ cd /root/git
 
 cd /root/opt
 
-# Wget some projects; it may be necessary to update the download links (especially for new versions)
+## Wget some projects; it may be necessary to update the download links (especially for new versions).
 
+#wget http://www.srware.net/downloads/iron64.deb - SRWare Iron is not working due to a dependency problem.
+#wget http://downloads.sourceforge.net/project/wxhexeditor/wxHexEditor/v0.22%20Beta/wxHexEditor-v0.22-src.tar.bz2
+#wget http://techpatterns.com/downloads/download_item.php?folder=firefox&filename=useragentswitcher.xml
 #wget wifipineapple.com/wp4.sh
 #wget https://pwn-star.googlecode.com/files/PwnSTAR_0.9.tgz
 #wget https://www.cookiecadger.com/files/CookieCadger-1.0.jar
 #wget http://www.sptoolkit.com/?aid=2755&sa=1
 #wget http://subterfuge.googlecode.com/files/SubterfugePublicBeta5.0.tar.gz
 
-# Subversion some projects...
+## Subversion some projects...
 
+#svn checkout http://fuzzdb.googlecode.com/svn/trunk/ fuzzdb
 #svn checkout http://proxmark3.googlecode.com/svn/trunk pm3
 
-# & Make Git requests for other projects.
+## & Make Git requests for other projects.
 
 #git clone git://git.kali.org/live-build-config.git
 #git clone https://github.com/zcutlip/bowcaster.git
+#git clone git://github.com/skysploit/simple-ducky.git simple-ducky
 
-# Now to untar stuff
+## Moving installers to /root/opt/installers
+
+#mv iron64.deb /root/opt/installers
+
+## Now to untar stuff
 
 #tar xvjf devkitARM_r41-i686-linux.tar.bz2 devkitpro/
 #tar xvzf PwnSTAR_0.9.tgz
 #tar xfvz Wi-fEye-v0.5.6.tar.gz
 #tar xfvz SubterfugePublicBeta5.0.tar.gz
 
-# Changing permissions on simple stuff
+## Changing permissions on simple stuff
 
 #chmod +x wp4.sh
 #chmod +x CookieCadger-1.0.jar
+#chmod +x /root/opt/installers/iron64.deb
+
+## Move a couple of things to where we want them.
+
+#mv useragentswitcher.xml /root/opt/configs
+
+## Installation of things
+
+#dpkg -i /root/opt/iron64.deb
 
 # Removing unneccessary files.
 
